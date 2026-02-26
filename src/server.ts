6bad6c953540eb8a8789f80683509947503c485f
iamsondev <sondipkumarsk@gmail.com>
2026-02-26 23:28:28 +0600

    fix the server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,16 @@
 import app from "./app.js";
 import { prisma } from "./lib/prisma.js";
 
-const PORT = process.env.PORT || 5000;
-
 prisma.$connect().catch((err) => {
   console.log("Database connection error", err);
 });
 
+const PORT = process.env.PORT || 5000;
+
 if (process.env.NODE_ENV !== "production") {
-  async function main() {
-    try {
-      await prisma.$connect();
-      console.log("Connected to database successfully");
-      app.listen(PORT, () => {
-        console.log(`server is running on http://localhost:${PORT}`);
-      });
-    } catch (err) {
-      console.log("An error occurred", err);
-      process.exit(1);
-    }
-  }
-  main();
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 }
 
 export default app;
