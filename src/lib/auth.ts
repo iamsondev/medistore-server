ab53ab7351fb9107b6453c903fa6aaff9f648cbb
iamsondev <sondipkumarsk@gmail.com>
2026-02-27 17:45:55 +0600

    fix: add google oauth redirectURI for production

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -138,6 +138,8 @@ export const auth = betterAuth({
       accessType: "offline",
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      redirectURI:
+        "https://medistore-server-fawn.vercel.app/api/auth/callback/google",
     },
   },
 
